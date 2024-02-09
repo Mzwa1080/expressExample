46da999f6e07f8119640d464ae9cc24de4289844
Mzwa1080 <mshawnlouw@gmail.com>
2024-02-09 16:15:51 +0200

    added the express and 3 routers

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("welcome to expressjs");
+import express  from "express";
+// create express app
+const app = express();
+const port = +process.env.PORT || 4000;
+
+// setup a router
+app.get('/', (req,res)=>{
+    res.json({
+        status : res.statusCode,
+        msg: 'You\'re home'
+    })
+})
+
+app.get('/about', (req,res)=>{
+    res.json({
+        status : res.statusCode,
+        msg: 'About page'
+    })
+})
+app.get('*', (req,res)=>{
+    res.json({
+        status : 404,
+        msg: '404 Page'
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`server running on http://localhost:${port}`);
+})
